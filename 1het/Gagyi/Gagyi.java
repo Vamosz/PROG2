7ae76c76a706f95b84f4b9524fea29dc220f7ae8
Vámosi Patrik <36470394+Vamosz@users.noreply.github.com>
2018-09-24 07:27:39 +0200

    Create Gagyi.java

---
+++ 1het/Gagyi/Gagyi.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class gagyi{
+
+public static void main(String[] args) throws IOException
+{
+	Integer x,t;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("kérlek add meg az x-et: ");
+		x=scan.nextInt();
+		
+		System.out.print("kérlek add meg az t-et: ");
+		t=scan.nextInt();
+		
+		while(x<=t && x>=t && t!=x){	
+		};
+
+
+
+}
+
+}
