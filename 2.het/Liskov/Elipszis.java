d6bff2f2d2016ed7f14c18d76619521e84359a2a
Vámosi Patrik <36470394+Vamosz@users.noreply.github.com>
2018-11-15 23:09:10 +0100

    Add files via upload

---
+++ 2.het/Liskov/Elipszis.java
@@ -0,0 +1,7 @@
+
+public class Elipszis extends Liskov{
+ 	 void tengely(){
+ 		System.out.print("Függoleges tengely modositva 10re.\nVizszintes tengely 20ra.\n");
+ 	}
+    
+}
---
+++ 2.het/Liskov/Kor.java
@@ -0,0 +1,5 @@
+
+public class Kor extends Elipszis{
+    // public void repul(){}
+    
+}
