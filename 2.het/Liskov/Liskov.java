045d20827c41973736fdd0c22bfaead959268d87
Vámosi Patrik <36470394+Vamosz@users.noreply.github.com>
2018-11-20 20:35:21 +0100

    Update Liskov.java

--- 2.het/Liskov/Liskov.java
+++ 2.het/Liskov/Liskov.java
@@ -2,7 +2,7 @@ public class Liskov{
 
     public static void main(String[] args) {
      
-    ValtoztatoElipszis ellipse = new ValtoztatoElipszis(); // Létrehozunk egy Elipszis objektumot
+    ValtoztatoEllipszis ellipse = new ValtoztatoEllipszis(); // Létrehozunk egy Elipszis objektumot
     Kor circle = new Kor(); // Illetve egy kört 
       
         
@@ -10,15 +10,15 @@ public class Liskov{
     }
     
     
-public static class Kor extends Elipszis{
+public static class Kor extends Ellipszis{
     
 }
-public static class ValtoztatoElipszis extends Elipszis{
+public static class ValtoztatoEllipszis extends Ellipszis{
     void tengely(){
  		System.out.print("Függoleges tengely modositva 10re.\nVizszintes tengely 20ra.\n");
  	}
 }
-public static class Elipszis extends Liskov{
+public static class Ellipszis extends Liskov{
  	 
     
 }
